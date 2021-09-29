459e07e5396d72fa1a55622de758d087f004345f
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-29 18:16:07 +0200

    Join_Queries1

--- Queries/Join_Queries/Join1.sql
+++ Queries/Join_Queries/Join1.sql
@@ -1,2 +1,6 @@
 -- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
+SELECT `students`.*
+FROM `students`
+JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
