b30e06ef6a131108b0d67ff0f595845c5e291979
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-29 23:01:00 +0200

    Join_Queries5

--- Queries/Join_Queries/Join5.sql
+++ Queries/Join_Queries/Join5.sql
@@ -1,1 +1,7 @@
 -- Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT `degrees`.`name`, `courses`.`name`, `teachers`.`name`, `teachers`.`surname`
+FROM `degrees`
+JOIN `courses` ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `course_teacher` ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id`;
