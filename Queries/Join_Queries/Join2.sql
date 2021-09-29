5ca5acec5d7a7e84f7a94a68d72b6b21acef0e44
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-29 18:24:51 +0200

    Join_Queries2

--- Queries/Join_Queries/Join2.sql
+++ Queries/Join_Queries/Join2.sql
@@ -1,1 +1,6 @@
 -- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT `degrees`.*
+FROM `degrees`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'
