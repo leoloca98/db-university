2baad47c282b01cc5196e4cbcffe9beb33401eda
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-29 22:42:23 +0200

    Join_Queries4

--- Queries/Join_Queries/join4.sql
+++ Queries/Join_Queries/join4.sql
@@ -1,2 +1,8 @@
 -- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT `students`.`registration_number`, `students`.`name`, `students`.`surname`, `degrees`.`name`, `degrees`.`level`, `departments`.`name`
+FROM `students`
+JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`, `students`.`name`;
